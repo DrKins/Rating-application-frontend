f6ac28f86b7233328e15e38326b0a2dc4a9b3053
DrKins <fade.sestan@gmail.com>
2020-06-20 01:56:30 +0200

    New design for reakcije.vue, restructured assets.

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,15 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
 *{
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Noto Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   margin: 0px;
   padding: 0px;
+  overflow: hidden;
 }
 
 body {
--- src/components/reakcije.vue
+++ src/components/reakcije.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="slike">
-  <img alt="hotel" src="../assets/hotel.webp" id="hotel">
-  <h1>Napuštate naš hotel nadamo se da ste imali lijepo iskustvo ostavite vašu reakciju</h1>
-   <img v-on:click="preusmjeri" alt="1 smajli" src="../assets/1.png"  class="emotikon" id="1">
-    <img v-on:click="preusmjeri" alt="2 smajli" src="../assets/2.png" class="emotikon" id="2">
-    <img v-on:click="preusmjeri" alt="3 smajli" src="../assets/3.png" class="emotikon" id="3">
-    <img v-on:click="preusmjeri" alt="4 smajli" src="../assets/4.png" class="emotikon" id="4">
-    <img v-on:click="preusmjeri" alt="5 smajli" src="../assets/5.png" class="emotikon" id="5">
+  <div class="pozadina">
+  <img alt="logotip" src="../assets/logo.png" class="logotip" id="logo">
+  <h1 class="poruka">Molimo Vas ocijenite naše usluge klikom na emotikon!</h1>
+   <img v-on:click="preusmjeri" alt="1 smajli" src="../assets/blackPack/1.png"  class="emotikon" id="1">
+    <img v-on:click="preusmjeri" alt="2 smajli" src="../assets/blackPack/2.png" class="emotikon" id="2">
+    <img v-on:click="preusmjeri" alt="3 smajli" src="../assets/blackPack/3.png" class="emotikon" id="3">
+    <img v-on:click="preusmjeri" alt="4 smajli" src="../assets/blackPack/4.png" class="emotikon" id="4">
+    <img v-on:click="preusmjeri" alt="5 smajli" src="../assets/blackPack/5.png" class="emotikon" id="5">
   <div class="reakcije"
   v-for="(reakcija,index) in reakcije"
   v-bind:item="reakcija" 
@@ -71,11 +71,21 @@ async created()
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.pozadina {
+  min-height: 100vh;
+}
+.logotip {
+ width: 25%;
+ padding: 4%;
+}
+.poruka {
+  padding: 1%;
+  font-size: 3.5vh;
+}
 .emotikon
 {
-  width: 6%;
-  height: 6%;
-  padding:15px;
+width: 4%;
+padding: 4%;
 }
 </style>
 
