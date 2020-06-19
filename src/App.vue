d2b2cb72de39978fd2be06f463d02235b6737db3
DrKins <fade.sestan@gmail.com>
2020-06-20 00:47:14 +0200

    Added new gradient background to app.vue

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,14 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  background:grey;
   margin: 0px;
   padding: 0px;
 }
+
+body {
+background: #0F2027;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to bottom, #2C5364, #203A43, #0F2027);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to bottom, #2C5364, #203A43, #0F2027); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+
 </style>
