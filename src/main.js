9e981b5c58a7bcbca0523a1bdedd5c25ea24f699
DrKins <fade.sestan@gmail.com>
2020-08-24 12:34:59 +0200

    bugfix v3.2

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ Vue.use(window.VueCharts);
 Vue.config.productionTip = false;
 
 // Socket.io configuration
-const socket = io('172.105.156.159:8005',{transports: ['websocket']});
+const socket = io('172.105.156.159:8006',{transports: ['websocket']});
 Vue.use(VueSocketIOExt, socket);
 new Vue({
   store,
